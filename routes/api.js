bd386890254578fce84a6a172e4e28262241e1f1
tokyosuite <helenyueyu@gmail.com>
2019-01-19 08:11:16 -0800

    First commit.

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var router = express.Router()
+
+// Models
+var Product = require('../models/product')
+
+// Routes
+Product.methods(['get', 'put', 'post', 'delete'])
+Product.register(router, '/products')
+
+router.get('/products', function(req, res){
+  res.send('api is working')
+})
+
+// Return router
+module.exports = router
